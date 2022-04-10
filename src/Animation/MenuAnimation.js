c18ea74be8cca7da3e95376bd36c9f68a187bed8
Michele Dellaquila <micheledellaquila2003@gmail.com>
2022-04-10 12:21:47 +0200

    minor fix

--- src/Animation/MenuAnimation.js
+++ src/Animation/MenuAnimation.js
@@ -1,7 +1,7 @@
 const MenuAnimation = {
-  hidden: { y: -1000, transition: { type: 'easeInCubic', duration: 700 } },
-  show: { y: 0, transition: { type: 'easeInCubic', duration: 700 } },
-  exit: { y: -1000, transition: { type: 'easeInCubic', duration: 700 } },
+  hidden: { y: -1000, transition: { type: 'easeInCubic', duration: 0.7 } },
+  show: { y: 0, transition: { type: 'easeInCubic', duration: 0.7 } },
+  exit: { y: -1000, transition: { type: 'easeInCubic', duration: 0.7 } },
 };
 
 export default MenuAnimation;
